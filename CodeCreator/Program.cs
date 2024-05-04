6932574c76cf7931225f07e2f5c20c9e72d14bc4
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-05-04 12:16:58 +0300

    Add project files.

---
+++ CodeCreator/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System.Security.Cryptography;
+using System.Text;
+
+const string CharSet = "ACDEFGHKLMNPRTXYZ234579";
+const int Length = 8;
+
+//Faster Than Random Class and Secure
+RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+string BuilderCode()
+{
+    byte[] randomBytes = new byte[Length];
+    rng.GetBytes(randomBytes);
+
+    StringBuilder code = new StringBuilder();
+    foreach (byte b in randomBytes)
+    {
+        int index = b % CharSet.Length;
+        code.Append(CharSet[index]);
+    }
+
+    return code.ToString();
+}
+
+bool CheckCode(string code)
+{
+    return !string.IsNullOrEmpty(code) && code.Length == Length && IsCodeValid(code);
+}
+
+bool IsCodeValid(string code)
+{
+    return code.All(c => CharSet.Contains(c)) ? true : false;
+}
+string generatedCode = BuilderCode();
+
+Console.WriteLine($"Code: {generatedCode}\r\n {(CheckCode(generatedCode) ? "Valid code" : "Not Valid code")}");
+Console.ReadLine();
