ea176b93c01390d09d15b6b7eeb121e3b93d54d2
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-05-05 21:20:18 +0300

    added XML documentation

--- CodeCreator/Program.cs
+++ CodeCreator/Program.cs
@@ -9,11 +9,18 @@ const int Length = 8;
 //Faster Than Random Class and Secure
 RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
 
+
+/// <summary>
+/// Generates a random code using a secure and fast method.
+/// </summary>
+/// <returns>A randomly generated code string.</returns>
 string BuilderCode()
 {
+    // Generate an array of random bytes.
     byte[] randomBytes = new byte[Length];
     rng.GetBytes(randomBytes);
 
+    // Build the code string using characters from the CharSet.
     StringBuilder code = new StringBuilder();
     foreach (byte b in randomBytes)
     {
@@ -24,11 +31,23 @@ string BuilderCode()
     return code.ToString();
 }
 
+
+/// <summary>
+/// Checks if a given code string is valid.
+/// </summary>
+/// <param name="code">The code string to be checked.</param>
+/// <returns>True if the code is valid, otherwise false.</returns>
 bool CheckCode(string code)
 {
     return !string.IsNullOrEmpty(code) && code.Length == Length && IsCodeValid(code);
 }
 
+
+/// <summary>
+/// Checks if a given code string consists only of characters from the CharSet.
+/// </summary>
+/// <param name="code">The code string to be checked.</param>
+/// <returns>True if all characters in the code are valid, otherwise false.</returns>
 bool IsCodeValid(string code)
 {
     return code.All(c => CharSet.Contains(c)) ? true : false;
